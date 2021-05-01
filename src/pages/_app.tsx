23f279798d4580152dff97dc0ff48dc4517dd240
Philipe de Souza Ferreira <philipesferreiraa@gmail.com>
2021-05-01 20:17:13 -0300

    Context API from player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,28 @@ import { Player } from '../components/Player';
 
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
+import { PlayerContext } from '../context/PlayerContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setepisodeList] = useState([]);
+  const [currentEpisodeIndex, setcurrentEpisodeIndex] = useState(0);
+
+  function play(episode) {
+    setepisodeList([episode]);
+    setcurrentEpisodeIndex(0);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import { GetStaticProps } from 'next';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import Image from 'next/image';
+import Link from 'next/link';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDuratinToTimeString';
+
+import styles from './home.module.scss';
+import Episode from './episodes/[slug]';
+import { useContext } from 'react';
+import { PlayerContext } from '../context/PlayerContext';
 
 type Episode = {
   id: string;
@@ -10,20 +17,99 @@ type Episode = {
   members: string;
   publishedAt: string;
   thumbnail: string;
-  description: string;
   duration: number;
   durationAsString: string;
   url: string;
 };
 
 type HomeProps = {
-  episodes: Episode[];
+  latesEpisodes: Episode[];
+  allEpisodes: Episode[];
 };
 
-export default function Home(props: HomeProps) {
+export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
-    <div>
-      <h1>Index</h1>;<p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2> Tocando agora </h2>
+        <ul>
+          {latesEpisodes.map((episode) => {
+            return (
+              <li key={episode.id}>
+                <Image
+                  width={130}
+                  height={130}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit='cover'
+                />
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type='button' onClick={() => play(episode)}>
+                  <img src='/play-green.svg' alt='Tocar Episódio' />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map((epsiode) => {
+              return (
+                <tr key={epsiode.id}>
+                  <td style={{ width: 100 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={epsiode.thumbnail}
+                      alt={epsiode.title}
+                      objectFit='cover'
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${epsiode.id}`}>
+                      <a>{epsiode.title}</a>
+                    </Link>
+                  </td>
+                  <td>{epsiode.members}</td>
+                  <td style={{ width: 100 }}>{epsiode.publishedAt}</td>
+                  <td>{epsiode.durationAsString}</td>
+                  <td>
+                    <button type='button'>
+                      <img src='/play-green.svg' alt='Tocar episódio' />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 }
@@ -46,7 +132,6 @@ export const getStaticProps: GetStaticProps = async () => {
         locale: ptBR,
       }),
       thumbnail: episode.thumbnail,
-      description: episode.description,
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(
         Number(episode.file.duration)
@@ -55,9 +140,14 @@ export const getStaticProps: GetStaticProps = async () => {
     };
   });
 
+  const latesEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
   return {
     props: {
       episodes,
+      latesEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8,
   };
